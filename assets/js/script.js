16793aa87e5f9f91d535a1a530f4279d0eeb8dd0
Raenique27 <raeniquewalters@gmail.com>
2022-08-04 17:08:17 -0400

    added functions to javascript and updated html as well as css

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,7 @@
-var quizQuestion = document.querySelector("#quiz-content");
+var quizQuestion = document.querySelector("#question-title");
 var highscoreTimer = document.querySelector("#highscore-timer");
 var timer = highscoreTimer.querySelector("p");
-var answerChoices = quizQuestion.querySelector("ul");
+var answerChoices = Array.from(document.querySelectorAll(".choice-text"));
 var results = document.querySelector("#results-content");
 var pageContent = document.querySelector("main");
 var highscoreContainer = document.querySelector("#highscore-container");
@@ -10,37 +10,67 @@ var initialsInput = highscoreContainer.querySelector("input");
 var submitbtn = highscoreContainer.querySelector("button");
 var highscorePage = document.querySelector("#highscore-page");
 
+var activeQuestion = {}
+var answerSelected = true
+var score = 0
+var questionCount = 0
+var otherQuestions = {}
 
 
 var questions = [ 
     {
     question: "Commonly used data types DO NOT include:",
-    answer: "3. alert",
-    options: ["1. string", "2. boolean", "3. alert", "4. number"]
+    options: ["1. string", "2. boolean", "3. alert", "4. number"],
+    answer: 3,
     
 },
 {
     question:"The condition in an if/else statement is enclosed with _______.",
-    answer: "3. parenthesis",
-    options: ["1. quotes", "2. curly brackets", "3. parenthesis", "4. square brackets"]
+    options: ["1. quotes", "2. curly brackets", "3. parenthesis", "4. square brackets"],
+    answer: 3,
 
 },
 {
     question: "Arrays in javascript can be used to store ______.",
-    answer: "4. all of the above",
-    options: ["1. numbers and strings", "2. booleans", "3. other arrays", "4. all of the above"]
+    options: ["1. numbers and strings", "2. booleans", "3. other arrays", "4. all of the above"],
+    answer: 4,
 },
 {
     question: "String values must be enclosed within ______ when being assigned to variables.",
-    answer: "2. quotes",
-    options:["1. commas", "2. quotes", "3. curly brackets", "4. parenthesis"]
+    options:["1. commas", "2. quotes", "3. curly brackets", "4. parenthesis"],
+    answer: 2,
 },
 {
     question: "A very useful tool used during development and debugging for printing content to the debugger is: ",
-    answer: "4. console.log",
-    options: ["1. Javascript", "2. terminal/bash", "3. for loops", "4. console.log"]
+    options: ["1. Javascript", "2. terminal/bash", "3. for loops", "4. console.log"],
+    answer: 4,
 },
 ];
+
+var points = 100
+var questionAmt = 5
+
+var startQuiz = function () {
+    questionCount = 0
+    score = 0
+    activeQuestion = [questions[i]]
+    nextQuestion()
+}
+
+var nextQuestion = function () {
+    if (activeQuestion.length === 0 || questionCount > questionAmt) {
+        localStorage.setItem("currentScore", score)
+
+        return window.location.assign("/highscores.html")
+    }
+
+    questionCount++
+
+    activeQuestion = otherQuestions[questions[i]]
+    questions.innerHtml = activeQuestion.questions
+
+    
+}
 
 
 
