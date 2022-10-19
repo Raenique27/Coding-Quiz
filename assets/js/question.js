aa669a328b5d813cf475ab3cfe829bb9f119d737
Raenique27 <raeniquewalters@gmail.com>
2022-10-18 20:20:14 -0400

    added questions to question.js file

---
+++ assets/js/question.js
@@ -0,0 +1,47 @@
+var questions = [
+    {
+        question: "Commonly used data types DO Not include:",
+        answers: [
+            {text: "strings", correct: false},
+            {text: "booleans", correct: false}, 
+            {text: "alerts", correct: true},
+            {text: "numbers", correct: false},
+        ]
+    },
+    {
+        question: "The condition in an if/else statement is enclosed with _________.",
+        answers: [
+            {text: "quotes", correct: false},
+            {text: "curly brackets", correct: false},
+            {text: "square brackets", correct: false},
+            {text: "parenthesis", correct: true},
+        ]
+    },
+    {
+        question: "Arrays in Javascript can be used to store _________.",
+        answers: [
+            {text: "numbers and strings", correct: false},
+            {text: "other arrays", correct: false},
+            {text: "strings", correct: false},
+            {text: "all of the above", correct: true},
+        ]
+    },
+    {
+        question: "String values must be enclosed within ________ when being assigned to variables.",
+        answers: [
+            {text: "commas", correct: false},
+            {text: "curly brackets", correct: false},
+            {text: "parenthesis", correct: false},
+            {text: "quotes", correct: true},
+        ]
+    },
+    {
+        question: "A very useful tool used during development and debugging for printing content to the debugger is: ",
+        answers: [
+            {text: "javscript", correct: false},
+            {text: "terminal/bash", correct: false},
+            {text: "console.log", correct: true},
+            {text: "for loops", correct: false},
+        ]
+    },
+];
